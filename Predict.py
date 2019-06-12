d67f465974d2a43a213505b7a5dd08b112933e50
zhaohehe <zhaohehe520@gmail.com>
2019-06-12 14:21:10 -0400

    "98% Accuracy on CNN"

--- Predict.py
+++ Predict.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+from tensorflow.keras.utils import normalize
 
 df_test = pd.read_csv("test.csv")
 test = df_test.iloc[:,:].values.astype("float32")
 test = test.reshape(test.shape[0],28,28)
-
-model = tf.keras.models.load_model("Mnist.model")
-predictions = model.predict(test)
+x_test = normalize(test, axis=1)
+x_test = np.array(x_test).reshape(-1,28,28,1)
+model = tf.keras.models.load_model("CNN_1")
+predictions = model.predict(x_test)
 
 n=1
 ID = []
@@ -20,4 +22,4 @@ res = pd.DataFrame()
 res["ImageId"] = ID
 res["Label"] = Label
 
-res.to_csv("Mnist.csv")
+res.to_csv("CNN.csv")
